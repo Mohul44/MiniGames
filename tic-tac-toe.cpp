63397d53ffe989cfa053d1c67e91f9937fca384b
Mohul <mohulmaheshwari4@gmail.com>
2021-08-15 23:08:02 +0530

    isWin

---
+++ tic-tac-toe.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define player1 1
+#define player2 2
+#define m 3
+#define n 3
+#define fr(i,n) for(int i=0;i<n;i++)
+
+bool isWin(vector<vector<int>> & matrix){
+    fr(i,m){
+        if(matrix[i][0]!=0 && matrix[i][0] == matrix[i][1] && matrix[i][2] == matrix[i][1])return true;
+    }
+    fr(i,n){
+        if(matrix[0][i]!=0 && matrix[0][i] == matrix[1][i] && matrix[2][i] == matrix[1][i])return true;
+    }
+    if(matrix[0][0]!=0 && matrix[0][0]==matrix[1][1] && matrix[1][1]==matrix[2][2])return true;
+    if(matrix[0][2]!=0 && matrix[0][2]==matrix[1][1] && matrix[1][1]==matrix[2][0])return true;
+    return false;
+}
