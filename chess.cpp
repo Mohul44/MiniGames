0091011da2a681b041d42668a7208afe26ddf41b
Mohul <mohulmaheshwari4@gmail.com>
2021-08-16 14:05:24 +0530

    chess

---
+++ chess.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include<typeinfo>
+using namespace std;
+class Piece;
+class Board;
+#define N 8
+int main(){  
+   //testPiece();
+ //  PlayGame();
+    return 0;
+}
