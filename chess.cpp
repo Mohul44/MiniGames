4c1b65d73ff651a7ae2c11a186b0740c4f376443
Mohul <mohulmaheshwari4@gmail.com>
2021-08-16 14:07:05 +0530

    playGame

--- chess.cpp
+++ chess.cpp
@@ -27,6 +27,135 @@ class Spot{
 
 };
 
+class Piece{
+    public:
+    bool isWhite;
+    bool isAlive;
+    bool isKing;
+    Piece()
+    {
+        isWhite = true;
+        isAlive = true;
+        isKing = false;
+    }
+
+    Piece(bool white)
+    {
+        isWhite = white;
+        isAlive = true;
+    }
+
+     Piece(bool white, bool king)
+    {
+        isWhite = white;
+        isAlive = true;
+        isKing = king;
+    }
+
+    void getKilled(){
+        isAlive = false;    
+    }
+
+    virtual bool canMove(Board & board, Spot * start, Spot * end) = 0;
+};
+
+class Knight: public Piece{
+    public:
+    Knight(bool white){
+        isWhite = white;
+     }
+    bool canMove(Board & board, Spot * start, Spot * end){
+        if(end->onSpotPiece!=NULL && start->onSpotPiece->isWhite==end->onSpotPiece->isWhite)return false;
+        int diff_x = abs(start->x-end->x);
+        int diff_y = abs(start->y-end->y);
+        if(diff_x * diff_y==2){
+            return true;
+        }
+        return false;
+    }
+};
+
+class Bishop: public Piece{
+    public:
+    Bishop(bool white){
+        isWhite = white;
+     }
+    bool canMove(Board & board, Spot * start, Spot * end){
+         if(end->onSpotPiece!=NULL && start->onSpotPiece->isWhite==end->onSpotPiece->isWhite)return false;
+        int diff_x = abs(start->x-end->x);
+        int diff_y = abs(start->y-end->y);
+        if(diff_x == diff_y){
+            return true;
+        }
+        return false;
+    }
+};
+
+class King: public Piece{
+    public:
+    King(bool white){
+        isWhite = white;
+        isKing = true;
+     }
+    bool canMove(Board & board, Spot * start, Spot * end){
+         if(end->onSpotPiece!=NULL && start->onSpotPiece->isWhite==end->onSpotPiece->isWhite)return false;
+        int diff_x = abs(start->x-end->x);
+        int diff_y = abs(start->y-end->y);
+        if(diff_x == diff_y && diff_x==1){
+            return true;
+        }
+        return false;
+    }
+};
+
+class Pawn: public Piece{
+    public:
+     Pawn(bool white){
+        isWhite = white;
+     }
+       bool canMove(Board & board, Spot * start, Spot * end){
+         if(end->onSpotPiece!=NULL && start->onSpotPiece->isWhite==end->onSpotPiece->isWhite)return false;
+        int diff_x = abs(start->x-end->x);
+        int diff_y = abs(start->y-end->y);
+        if(diff_y==0 && diff_x==1){
+            return true;
+        }
+        return false;
+    }
+};
+
+class Rook: public Piece{
+    public:
+    Rook(bool white){
+        isWhite = white;
+     }
+    bool canMove(Board & board, Spot * start, Spot * end){
+         if(end->onSpotPiece!=NULL && start->onSpotPiece->isWhite==end->onSpotPiece->isWhite)return false;
+        int diff_x = abs(start->x-end->x);
+        int diff_y = abs(start->y-end->y);
+        if((diff_x == 0 && diff_y>0) || (diff_y == 0 && diff_x>0)){
+            return true;
+        }
+        return false;
+    }
+};
+
+
+class Queen: public Piece{
+    public:
+    Queen(bool white){
+        isWhite = white;
+     }
+      bool canMove(Board & board, Spot * start, Spot * end){
+         if(end->onSpotPiece!=NULL && start->onSpotPiece->isWhite==end->onSpotPiece->isWhite)return false;
+        int diff_x = abs(start->x-end->x);
+        int diff_y = abs(start->y-end->y);
+        if((diff_x == 0 && diff_y>0) || (diff_y == 0 && diff_x>0) || diff_x == diff_y){
+            return true;
+        }
+        return false;
+    }
+};
 
 int main(){  
    //testPiece();
