a9f5cbbc20a372640db760df16420ee40ce572a6
Mohul <mohulmaheshwari4@gmail.com>
2021-08-16 14:06:29 +0530

    spot

--- chess.cpp
+++ chess.cpp
@@ -5,6 +5,29 @@ using namespace std;
 class Piece;
 class Board;
 #define N 8
+
+class Spot{
+    public:
+    int x;
+    int y;
+    Piece * onSpotPiece;
+
+    Spot(){
+        x = -1;
+        y = -1;
+        onSpotPiece = NULL;
+    }
+
+    Spot(int a,int b, Piece * p)
+    {
+        x = a;
+        y = b;
+        onSpotPiece = p;
+    }
+
+};
+
+
 int main(){  
    //testPiece();
  //  PlayGame();
